3ee4abf2cf93ceac1f5c35a461e1e1080e49821a
annacrespo12 <annacrespo12@gmail.com>
2017-09-18 16:46:49 -0700

    play

--- cards.js
+++ cards.js
@@ -5,10 +5,23 @@ const fs = require("fs");
 
 console.log(basicCard);
 console.log(ClozeCard);
-inquirer.prompt(
-    [
-        /* Pass your questions in here */])
+inquirer.prompt([
+    {
+        message: "What game would you like to play?",
+        type: "checkbox",
+        choices: ["Basic Card", "Cloze Card", "quit"],
+        name: "startGame"
+    }
+])
     .then(function (answers) {
-        // Use user feedback for... whatever!!
+        var game = answers.startGame[0];
+        if (answers.startGame[0] === "Basic Card") {
+            play("basic-card.json");
+        } else if (answers.startGame[1] === "Cloze Card") {
+            play("ClozeCard.json");
+    } else if (answers.startGame[3] === "quit") {
+            quit();
+    }
+
     });
 
