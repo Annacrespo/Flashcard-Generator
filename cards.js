880838a87b9f2e10f65797242001f514756bacda
annacrespo12 <annacrespo12@gmail.com>
2017-09-18 20:14:46 -0700

    basicPlay

--- cards.js
+++ cards.js
@@ -3,25 +3,39 @@ const ClozeCard = require("./ClozeCard.js");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+var correct;
+var incorrect;
+var cardArray = [];
+
 console.log(basicCard);
 console.log(ClozeCard);
 inquirer.prompt([
     {
         message: "What game would you like to play?",
         type: "checkbox",
-        choices: ["Basic Card", "Cloze Card", "quit"],
+        choices: ["Basic Card", "Cloze Card", "Quit"],
         name: "startGame"
     }
-])
-    .then(function (answers) {
+]).then(function (answers) {
         var game = answers.startGame[0];
         if (answers.startGame[0] === "Basic Card") {
-            play("basic-card.json");
+            basicPlay("basic-card.json");
         } else if (answers.startGame[1] === "Cloze Card") {
-            play("ClozeCard.json");
+            basicPlay("ClozeCard.json");
     } else if (answers.startGame[3] === "quit") {
             quit();
     }
 
     });
 
+function basicPlay(basicCard, x) {
+    fs.readFile(basic-card.json, "utf8", function(error, data) {
+        if (error) {
+            console.log("Error!");
+        }
+    })
+}
+
+function quit() {
+
+}
